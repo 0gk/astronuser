12662629d80d728f97b2c4f758ef40d79480154e
0gk <n@rlab.ru>
2023-05-05 22:05:07 +0300

    test

--- options.lua
+++ options.lua
@@ -1,12 +1,12 @@
 -- set vim options here (vim.<first_key>.<second_key> = value)
 return {
   opt = {
-    mouse = '',
     relativenumber = true, -- sets vim.opt.relativenumber
     number = true, -- sets vim.opt.number
     spell = false, -- sets vim.opt.spell
     signcolumn = "auto", -- sets vim.opt.signcolumn to auto
     wrap = false, -- sets vim.opt.wrap
+    mouse = '',
   },
   g = {
     -- icons_enabled = false,
